0206438d056ad8f4b5cdb1993dc930ae61d81e96
Zain Ali <zainali187483@gmail.com>
2025-07-26 22:40:43 +0500

    feat: add navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { ArrowRight } from "lucide-react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="fixed z-5 w-full backdrop-blur-2xl flex justify-between
+items-center py-3 px-4 sm:px-20 x1l:px-32"
+    >
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="w-32 sm:w-44 cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
+        Get started
+        <ArrowRight className="w-4 h-4" />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
