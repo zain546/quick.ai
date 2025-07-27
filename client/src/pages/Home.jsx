d12ca6e996f89d585c4b89e325f6c7fe2de6cb4a
Zain Ali <zainali187483@gmail.com>
2025-07-27 17:28:40 +0500

    feat: create testimonials component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AiTools from "../components/AiTools";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar/>
       <Hero/>
       <AiTools/>
+      <Testimonial/>
     </>
   );
 };
