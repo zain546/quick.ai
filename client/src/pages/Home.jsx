e52648b1f0de44f2fb18ab46c6fe345214fc523b
Zain Ali <zainali187483@gmail.com>
2025-07-27 18:08:09 +0500

    feat: create foooter component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import AiTools from "../components/AiTools";
 import Testimonial from "../components/Testimonial";
 import Plan from "../components/Plan";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <AiTools/>
       <Testimonial/>
       <Plan/>
+      <Footer/>
     </>
   );
 };
