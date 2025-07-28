243b053a594ceb9f20c31bb5adaa0cf59c5c579b
Zain Ali <zainali187483@gmail.com>
2025-07-28 18:44:35 +0500

    feat: add animated marquee component for company logos

---
+++ client/src/components/CompanyLogoMarquee.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const CompanyLogoMarquee = () => {
+  const companies = [
+    "slack",
+    "framer",
+    "netflix",
+    "google",
+    "linkedin",
+    "instagram",
+    "facebook",
+    "slack",
+    "framer",
+    "netflix",
+    "google",
+    "linkedin",
+    "instagram",
+    "facebook",
+  ];
+
+  return (
+    <div className="overflow-hidden w-full relative max-w-5xl mx-auto select-none top-20">
+      {/* Left gradient */}
+      <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-[#ffffff] to-transparent"></div>
+
+      {/* Marquee scrolling logos */}
+      <div
+        className="marquee-inner flex will-change-transform min-w-[200%]"
+        style={{ animationDuration: "30s" }}
+      >
+        <div className="flex">
+          {companies.map((company, index) => (
+            <img
+              key={index}
+              alt={company}
+              className="w-full h-full object-cover mx-6"
+              draggable="false"
+              src={`https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/companyLogo/${company}.svg`}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Right gradient */}
+      <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-[#ffffff] to-transparent"></div>
+    </div>
+  );
+};
+
+export default CompanyLogoMarquee;
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
+import CompanyLogoMarquee from "./CompanyLogoMarquee";
 const Hero = () => {
   const navigate = useNavigate();
   return (
@@ -31,6 +32,10 @@ const Hero = () => {
       <div className="flex items-center gap-4 mt-8 mx-auto text-gray-600">
         <img src={assets.user_group} alt="" className="h-8" /> Trusted by 10k+
         people
+      </div>
+      {/* CompanyLogoMarquee */}
+      <div>
+        <CompanyLogoMarquee/>
       </div>
     </div>
   );
