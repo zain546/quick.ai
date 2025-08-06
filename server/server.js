4e8d96f4dbf90fb542b550a52652647d06494744
Zain Ali <zainali187483@gmail.com>
2025-08-06 18:17:39 +0500

    feat: Integrate Clerk authentication middleware and update dependencies

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
+
+app.use(requireAuth()); // Protect all routes
 
 const port = process.env.PORT || 5000;
 
