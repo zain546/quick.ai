a18a95a0de2cc12589ceb5c8022b7c5ebe771684
Zain Ali <zainali187483@gmail.com>
2025-08-06 12:39:15 +0500

    feat: Implement UI for remove object page

--- client/src/pages/RemoveObject.jsx
+++ client/src/pages/RemoveObject.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import { Scissors, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
 const RemoveObject = () => {
+  const [input, setInput] = useState("");
+  const [object, setObject] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>RemoveObject</h1>
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* left column */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Object Removal</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Upload Image</p>
+        <input
+          onChange={(e) => setInput(e.target.files[0])}
+          type="file"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          accept="image/*"
+          required
+        />
+        <p className="mt-6 text-sm font-medium">Describe object name to remvove</p>
+        <textarea
+          onChange={(e) => setObject(e.target.value)}
+          value={object}
+          rows={4}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 "
+          placeholder="e.g., watch or spoon, Only single object name"
+          required
+        />
+
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm cursor-pointer rounded-md">
+          <Scissors className="w-5" />
+          Remove object
+        </button>
+      </form>
+      {/* Right column */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+        <div className="flex items-center gap-3 mb-4">
+          <Scissors className="w-5 h-5 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Processed Image</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Scissors className="w-9 h-9" />
+            <p>Upload an image and click "Remove Object" to get started</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
