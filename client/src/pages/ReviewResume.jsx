1d82209a279ec57896debb3e492ed12365fd5787
Zain Ali <zainali187483@gmail.com>
2025-08-06 12:48:09 +0500

    feat: Implement UI of review resume page

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { FileText, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
 const ReviewResume = () => {
+  const [input, setInput] = useState("");
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-        <h1>ReviewResume</h1>
-    </div>
-  )
-}
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* left column */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#00DA83]" />
+          <h1 className="text-xl font-semibold">Resume Review</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Upload Resume</p>
+        <input
+          onChange={(e) => setInput(e.target.files[0])}
+          type="file"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          accept="application/pdf"
+          required
+        />
+        <p className="test-xs text-gray-500 font-light mt-1">
+          Supports PDF formats only.
+        </p>
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm cursor-pointer rounded-md">
+          <FileText className="w-5" />
+          Review Resume
+        </button>
+      </form>
+      {/* Right column */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+        <div className="flex items-center gap-3 mb-4">
+          <FileText className="w-5 h-5 text-[#00DA83]" />
+          <h1 className="text-xl font-semibold">Analysis Results</h1>
+        </div>
 
-export default ReviewResume
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <FileText className="w-9 h-9" />
+            <p>Upload a resume and click "Review Resume" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewResume;
