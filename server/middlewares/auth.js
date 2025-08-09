e72035f8755fec0acf581314049fb6fffc20a412
Zain Ali <zainali187483@gmail.com>
2025-08-09 10:23:28 +0500

    feat: Add authentication middleware to check user plan status

---
+++ server/middlewares/auth.js
@@ -0,0 +1,26 @@
+// Middleware to check if user is authenticated and hasPremiumPlan
+import { clerkClient } from "@clerk/express";
+
+export const auth = async (req, res, next) => {
+  try {
+    const { userId, has } = await req.auth();
+    const hasPremiumPlan = await has({ plan: "premium" });
+
+    const user = await clerkClient.users.getUser(userId);
+    if (!hasPremiumPlan && user.publicMetadata.free_usage) {
+      req.free_usage = user.privateMetadata.free_usage;
+    } else {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: 0,
+        },
+      });
+      req.free_usage = 0;
+    }
+    req.plan = hasPremiumPlan ? "premium" : "free";
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(error.status).json({ message: error.message });
+  }
+};
